6c4d6b71c4ebb98d90c3bd0f595d7203d6ac9f6d
Sasha Hart <sashanicolehart@gmail.com>
2018-08-24 04:52:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,13 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles' ];
 
 function addElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return array;
+  var newArray = [element, ...array];
+  return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var newArray = [element, ...array];
-  return newArray;
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
