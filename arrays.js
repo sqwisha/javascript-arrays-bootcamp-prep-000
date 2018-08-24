db987da0c4702013edad49bfe90e5e97913973b0
Sasha Hart <sashanicolehart@gmail.com>
2018-08-24 05:05:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  var endAddedArray = array.push(element);
+  var endAddedArray = []
   return endAddedArray;
 }
 
